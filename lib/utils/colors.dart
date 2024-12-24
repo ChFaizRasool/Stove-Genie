afba5b9a6cd9940e09c0c0e63ded7505192df5c3
HafizAbdulSaboor <abdulsaboor1956@gmail.com>
2024-12-24 15:29:38 +0500

    chnages

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color buttonColor = Color(0xffFFA307);
+  static const Color blurCOlor = Color(0xff09A5DC1A);
+  static const Color textColor = Color(0xffFF9C00);
+
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+}
