b143d9554e60ad90b0f8b880a41d53f6f56f2b3e
HafizAbdulSaboor <abdulsaboor1956@gmail.com>
2024-12-26 18:59:13 +0500

    changes

---
+++ lib/pages/fuck/fuck_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FuckScreen extends StatelessWidget {
+  const FuckScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
